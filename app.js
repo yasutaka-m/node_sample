ad9a28607efa26bccf39401fb0157414d5a23eb3
yasutaka-m_1 <y.matsuwaki@gmail.com>
2012-02-14 09:54:36 +0000

    "hello facebook"

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var fcgiApp = require("./fcgi"),
 var myServer = http.createServer(function(req, res) {
 	setTimeout(function() {
 		res.writeHead(200, {"Content-type": "text/html"});
-		res.end("hello world" + Date.now());
+		res.end("hello facebook" + Date.now());
 		console.log("Wrote response.");
 	}, 1000);
 	//throw new Error("Bollocks.");
